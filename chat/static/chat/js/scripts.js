272fa38df17e1a6d2b3a2278574b780cc4f7c8cd
Nerechka <sill@mail.ru>
2018-02-18 16:16:46 +0300

    Add enter-send

--- chat/static/chat/js/scripts.js
+++ chat/static/chat/js/scripts.js
@@ -2,3 +2,11 @@ $(document).ready(function() {
 const scrollDown = document.getElementById("frameScroll");
 scrollDown.scrollTop = scrollDown.scrollHeight;
 });
+
+$(document).ready(function(){
+	     	 $("#goMessage").keypress(function(e){
+	     	   if(e.keyCode==13){
+          $("#buttonGo").click();
+	     	   }
+	     	 });
+	     });
